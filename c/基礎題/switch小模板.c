1a84f09957356c564f92b08dc671c7161fcee6fc
Ycwei <iixun01200.tw@gmail.com>
2025-10-12 11:08:52 +0800

    Create switch小模板.c

---
+++ c/基礎題/switch小模板.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int ID;
+    scanf("%d",&ID);
+    switch(ID){
+        case 12:printf("Sam\n");break;
+        default:printf("Nothing\n");break;
+        
+    }
+
+    return 0;
+}
