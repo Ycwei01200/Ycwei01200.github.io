af70578dd3c284dc0565f4315b3928c984563726
Ycwei <iixun01200.tw@gmail.com>
2025-11-06 09:32:25 +0800

    Create day2_data_type.py

---
+++ python自學/day2_data_type.py
@@ -0,0 +1,15 @@
+a = 100
+b = 100.10
+c = 255
+d = "Hello World"
+cc ="255"
+aa ="100"
+
+print(a/b)
+print(c/b)
+print(d)
+print(type(d))
+print(float(a/b))
+print(int(c/b))
+print(int(cc,base=16)) #只有string 轉 int 可以這樣轉
+print(int(aa,base=8))
