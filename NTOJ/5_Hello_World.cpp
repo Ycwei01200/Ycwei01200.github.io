02db7fd5b002ab2072d7e05778ec53a943d1909e
Ycwei01200 <iixun01200.tw@gmail.com>
2025-11-17 17:59:31 +0800

    5_Hello_World.cpp updated

---
+++ NTOJ/5_Hello_World.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    string s;getline(cin,s);
+    cout << "Hello ,"<<s<<" !\n";
+    return 0;
+}
